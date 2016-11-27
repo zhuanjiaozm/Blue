272eb2ae434481a139adb6a53e5374f2a13f6479
乔均 <602763799@qq.com>
2016-11-27 22:25:49 +0800

    申购单（待审核）  未完成

---
+++ function/purchasing_requisition_list/purchasing_requisition_list.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', []);
+app.controller('purchasing_requisition_list-controller', function($scope, $http) {
+	$http({
+			url: '../../virtual_data/purchasing_requisition_list/purchasing_requisition_list.json',
+			method: 'GET'
+		})
+		.success(function(data, header, config, status) {
+			$scope.purchasingRequisitionList = data.data;
+			console.log("purchasingRequisitionList：",$scope.purchasingRequisitionList);
+		})
+		.error(function(data, header, config, status) {
+			//处理响应失败
+		});
+});
+angular.bootstrap(document, ['app']);
