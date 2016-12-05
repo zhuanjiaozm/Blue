c145f4683d2f6c7889882090d75353ffd96713d0
zhuanjiaozm <602763799@qq.com>
2016-12-05 18:30:52 +0800

    初次对接

--- function/login/login.js
+++ function/login/login.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
-    $(".login-button").click(function() {
-      window.location.href = "../../function/home/index.html";
-    });
-});
+var app = angular.module('app', []);
+app.controller('login', function($scope, $http) {
+	$scope.login = function(user) {
+		$scope.data={
+			errMsg:''
+		};
+		$http({
+			method: 'POST',
+			url: '/YunERP/implLogin',
+			data: user
+		}).success(function(data, status, headers, config) {
+			if(data.userinfo){
+				window.location.href="../home/index.html";
+			}
+			else{
+				$scope.data.errMsg = '系统错误，请联系管理员！';
+			}
+		}).error(function(data, status, headers, config) {
+			$scope.data.errMsg = '用户登录发生网络错误';
+		});
+	}
+})
--- function/purchasing_requisition/purchasing_requisition.js
+++ function/purchasing_requisition/purchasing_requisition.js
@@ -1,100 +1,137 @@
 var app = angular.module('app', []);
 app.controller('purchasing-requisition-controller', function($scope, $http) {
-	//默认可以点击添加商品按钮
-	$scope.added = true;
+    //默认可以点击添加商品按钮
+    $scope.goodList = [];
+    $scope.goodList.push({
+        id: null,
+        good: {
+            product_code: null,
+            product_name: ''
+        },
+        accout: null,
+        remark: ''
+    });
+    $scope.addGoods = function() {
+        $scope.goodList.unshift({
+            id: null,
+            good: {
+                id: null,
+                name: ''
+            },
+            accout: null,
+            remark: ''
 
-	//商品列表  页面加载完成后从服务端获取
-	$http({
-			url: '../../virtual_data/purchasing_requisition/goodList.json',
-			method: 'GET'
-		})
-		.success(function(data, header, config, status) {
-			$scope.goodList = data.context;
-		})
-		.error(function(data, header, config, status) {
-			//处理响应失败
-		});
+        });
+    };
+    $scope.deleteGoods = function(goods) {
 
-	$scope.addGoods = function() {
-		//设置添加商品按钮为不可点击
-		$scope.added = false;
+        $('#my-confirm').modal({
+            onConfirm: function(options) {
+                $scope.goodList.splice($scope.goodList.indexOf(goods), 1);
+                $scope.$apply($scope);
+                //console.info("商品列表:", $scope.goodList);
+            }
+        });
+    };
 
-		//如果商品列表为空则新建
-		if(!$scope.goodList) {
-			$scope.goodList = [];
-		}
-		$scope.goodList.unshift({
-			id: null,
-			good: {
-				id: null,
-				name: ''
-			},
-			accout: null,
-			remark: '',
-			editEnable: true
-		});
-	}
-	$scope.deleteGoods = function(goods) {
-		if(!goods.id) {
-			//当删除新增商品后即可以点击添加商品按钮
-			$scope.added = true;
-		}
-		$('#my-confirm').modal({
-			onConfirm: function(options) {
-				$scope.goodList.splice($scope.goodList.indexOf(goods), 1);
-				$scope.$apply($scope);
-				//console.info("商品列表:", $scope.goodList);
-			}
-		});
-	}
-	$scope.editGoods = function(goods) {
-		goods.editEnable = !goods.editEnable;
-		if(!$scope.added) {
-			$scope.added = true;
-		}
-	}
 
-	var itemSelectedForSearchGoodsIndex = 0;
-	//选择商品  开始
-	$scope.selectGoods = function(index) {
-		itemSelectedForSearchGoods = index;
-		console.log("序号:", index);
-		//查询选择商品时的基本信息
-		$http({
-				url: '../../virtual_data/purchasing_requisition/select_goods.json',
-				method: 'GET'
-			})
-			.success(function(data, header, config, status) {
-				$scope.categoryList = data.category.categoryList;
-				console.log("$scope.categoryList:",$scope.categoryList);
-				$scope.recentSearch = data.quickSerch.recentSerch;
-			})
-			.error(function(data, header, config, status) {
-				//处理响应失败
-			});
-		$("#select-goods").modal();
-	}
+    var itemSelectedForSearchGoodsIndex = 0;
+    //选择商品  开始
+    $scope.selectGoods = function(index) {
+        itemSelectedForSearchGoods = index;
+        //商品列表  页面加载完成后从服务端获取
+        $http({
+            method: 'POST',
+            url: '/YunERP/implProductTypeList',
+            data: {}
+        }).success(function(data, status, headers, config) {
+            $scope.categoryList = data.list;
+            //  console.log(data);
+            $scope.recentSearch = data.hotlist;
+            $scope.allCategory = [];
+            angular.forEach(data.list, function(data, index, array) {
+                if (data.productlist.length > 0) {
+                    angular.forEach(data.productlist, function(obj, index, array) {
+                        $scope.allCategory.push(obj);
+                    });
+                }
+            });
+            //  console.log("所有商品列表：", $scope.allCategory);
+        }).error(function(data, status, headers, config) {
+            console.error('获取商品分类发生网络错误');
+        });
+        $("#select-goods").modal();
+    };
 
-	//点击搜索结果关闭弹出框
-	$scope.selectedGoods = function(selectGodos) {
-		//		console.log("序号:", itemSelectedForSearchGoods);
-		//		console.log("商品：",selectGodos);
-		$scope.goodList[itemSelectedForSearchGoods].good = selectGodos;
-		//保存商品名称完成后不可编辑当前商品
-		$scope.goodList[itemSelectedForSearchGoods].editEnable = false;
-		$("#select-goods").modal('close');
-	}
-	//清空最近搜索过的商品
-	$scope.clearSearchRecently=function(){
-		$scope.recentSearch=[];
-	}
-	//设置商品分类
-	$scope.setCategory=function(liIndex){
-		$scope.currentIndex=liIndex;
-	}
-	
-	
-	//选择商品  结束
+    $scope.searchGoodsByKeyWord = function(keyword) {
+        //console.log("搜索商品关键字：", keyword);
+        $scope.searchResult = [];
+        if (keyword.trim().length < 1) return;
+        angular.forEach($scope.allCategory, function(data, index, array) {
+            if ((data.product_name.indexOf(keyword.trim()) > -1) || (data.product_code.indexOf(keyword.trim()) > -1)) {
+                $scope.searchResult.push(data);
+            }
+        });
+    };
+    $scope.closeModal = function() {
+        $("#select-goods").modal('close');
+    };
 
+    //点击搜索结果关闭弹出框
+    $scope.selectedGoods = function(selectGodos) {
+        $scope.goodList[itemSelectedForSearchGoods].good = selectGodos;
+        console.log("选择的商品是：", selectGodos);
+        //保存商品名称完成后不可编辑当前商品
+
+        $("#select-goods").modal('close');
+    };
+    //清空最近搜索过的商品
+    $scope.clearSearchRecently = function() {
+        $scope.recentSearch = [];
+    };
+    //设置商品分类
+    $scope.setCategory = function(liIndex) {
+        $scope.currentIndex = liIndex;
+    };
+
+    //选择商品  结束
+
+    //获取可选项目列表
+    $http({
+        method: 'POST',
+        url: '/YunERP/implProjectListByUser',
+        data: {}
+    }).success(function(data, status, headers, config) {
+        $scope.projectList = data.list;
+        //$('#project-select').selected();
+        console.log('获取可选项目列表', data.list);
+    }).error(function(data, status, headers, config) {
+        console.error('获取可选项目列表发生网络错误');
+    });
+
+
+
+
+    //保存采购申请
+    $scope.save = function() {
+        angular.forEach($scope.goodList, function(data, index, array) {
+            $scope.goodList[index].product_id = data.good.product_id;
+            $scope.goodList[index].good = null;
+        });
+        $http({
+                method: 'POST',
+                url: '/YunERP/implSubmitPuarchsePlan',
+                data: {
+                    "list": $scope.goodList,
+                    "form": $scope.theForm
+                }
+            })
+            .success(function(data, status, headers, config) {
+
+            })
+            .error(function(data, status, headers, config) {
+                console.error('获取商品分类发生网络错误');
+            });
+    };
 });
 angular.bootstrap(document, ['app']);
